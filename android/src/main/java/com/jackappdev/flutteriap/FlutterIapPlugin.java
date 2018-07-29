9fead94f456194a797944451057e68aaadc5c86b
Jack ODonnell <jackappdevinc@gmail.com>
2018-07-29 04:11:07 -0400

    add IAPPurchase, return server verification info for android

--- android/src/main/java/com/jackappdev/flutteriap/FlutterIapPlugin.java
+++ android/src/main/java/com/jackappdev/flutteriap/FlutterIapPlugin.java
@@ -25,176 +25,168 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
  * FlutterIapPlugin
  */
 public class FlutterIapPlugin implements MethodCallHandler {
-    private final Activity activity;
-    private BillingManager billingManager;
+  private final Activity activity;
+  private BillingManager billingManager;
 
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_iap");
-        channel.setMethodCallHandler(new FlutterIapPlugin(registrar.activity()));
-    }
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_iap");
+    channel.setMethodCallHandler(new FlutterIapPlugin(registrar.activity()));
+  }
 
+  private FlutterIapPlugin(final Activity activity) {
+    this.activity = activity;
+    activity.getApplication().registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+      @Override
+      public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
 
-    private FlutterIapPlugin(final Activity activity) {
-        this.activity = activity;
-        activity.getApplication().registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
-            @Override
-            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+      }
 
-            }
+      @Override
+      public void onActivityStarted(Activity activity) {
 
-            @Override
-            public void onActivityStarted(Activity activity) {
+      }
 
-            }
+      @Override
+      public void onActivityResumed(Activity activity) {
 
-            @Override
-            public void onActivityResumed(Activity activity) {
+      }
 
-            }
+      @Override
+      public void onActivityPaused(Activity activity) {
 
-            @Override
-            public void onActivityPaused(Activity activity) {
+      }
 
-            }
+      @Override
+      public void onActivityStopped(Activity activity) {
 
-            @Override
-            public void onActivityStopped(Activity activity) {
+      }
 
-            }
+      @Override
+      public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
 
-            @Override
-            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+      }
 
-            }
-
-            @Override
-            public void onActivityDestroyed(Activity activity) {
-                if (billingManager != null) {
-                    billingManager.destroy();
-                    billingManager = null;
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onMethodCall(final MethodCall call, final Result result) {
+      @Override
+      public void onActivityDestroyed(Activity activity) {
         if (billingManager != null) {
-            billingManager.destroy();
-            billingManager = null;
+          billingManager.destroy();
+          billingManager = null;
         }
+      }
+    });
+  }
 
-
-        if (call.method.equals("inventory")) {
-            billingManager =
-                new BillingManager(activity, new BillingManager.BillingUpdatesListener() {
-                    @Override
-                    public void onBillingClientSetupFinished() {
-                    }
-
-                    @Override
-                    public void onConsumeFinished(String token,
-                                                  @BillingClient.BillingResponse int result) {
-                        Log.e("token", token);
-                    }
-
-                    @Override
-                    public void onPurchasesUpdated(List<Purchase> purchases) {
-                        StringBuilder sb = new StringBuilder("[");
-
-                        if (purchases != null) {
-                            for (Purchase purchase : purchases) {
-                                if (sb.length() > 1) {
-                                    sb.append(",");
-                                }
-                                sb.append("\"" + purchase.getSku() + "\"");
-                            }
-                        }
-
-                        sb.append("]");
-                        result
-                            .success("{\"status\":\"loaded\",\"skus\":" + sb.toString() + "}");
-
-                    }
-                }, null);
-        } else if (call.method.equals("buy")) {
-            billingManager =
-                new BillingManager(activity, new BillingManager.BillingUpdatesListener() {
-                    @Override
-                    public void onBillingClientSetupFinished() {
-                        billingManager.initiatePurchaseFlow(
-                            (String) call.arguments,
-                            BillingClient.SkuType.INAPP);
-                    }
-
-                    @Override
-                    public void onConsumeFinished(String token,
-                                                  @BillingClient.BillingResponse int result) {
-                        Log.e("token", token);
-                    }
-
-                    @Override
-                    public void onPurchasesUpdated(List<Purchase> purchases) {
-                        Log.e("purchases", purchases.toString());
-                        if (purchases.size() > 0) {
-                            Purchase p = purchases.get(0);
-                            Log.e("Consuming", p.getSku());
-                            billingManager.consumeAsync(p.getPurchaseToken());
-                            result.success(jsonFromString("purchased"));
-                        }
-                    }
-                }, null);
-        } else if (call.method.equals("fetch")) {
-            billingManager = new BillingManager(
-                activity,
-                new BillingManager.BillingUpdatesListener() {
-                    @Override
-                    public void onBillingClientSetupFinished() {
-                        billingManager.querySKUProducts((List<String>) call.arguments);
-
-                    }
-
-                    @Override
-                    public void onConsumeFinished(String token, int result) {
-
-                    }
-
-                    @Override
-                    public void onPurchasesUpdated(List<Purchase> purchases) {
-
-                    }
-                },
-                new SkuDetailsResponseListener() {
-                    @Override
-                    public void onSkuDetailsResponse(int responseCode,
-                                                     List<SkuDetails> skuDetailsList) {
-                        StringBuilder sb = new StringBuilder("[");
-                        if (skuDetailsList != null) {
-                            for (SkuDetails details : skuDetailsList) {
-                                if (sb.length() > 1) {
-                                    sb.append(",");
-                                }
-                                sb.append("{\"localizedDescription\":\"" + details
-                                    .getDescription() + "\"");
-                                sb.append(",\"localizedTitle\":\"" + details.getTitle() + "\"");
-                                sb.append(",\"price\":\"" + details.getPrice() + "\"");
-                                sb.append(",\"priceLocale\":\"" + details
-                                    .getPriceCurrencyCode() + "\"");
-                                sb.append(",\"localizedPrice\":\"" + details.getPrice() + "\"");
-                                sb.append(",\"productIdentifier\":\"" + details.getSku() + "\"}");
-                            }
-                        }
-                        sb.append("]");
-                        result
-                            .success("{\"status\":\"loaded\",\"products\":" + sb.toString() + "}");
-                    }
-
-                }
-            );
-        }
+  @Override
+  public void onMethodCall(final MethodCall call, final Result result) {
+    if (billingManager != null) {
+      billingManager.destroy();
+      billingManager = null;
     }
 
-    public String jsonFromString(String status) {
-        return "{\"status\":\"" + status + "\"}";
+    if (call.method.equals("inventory")) { // gets skus
+      billingManager = new BillingManager(activity, new BillingManager.BillingUpdatesListener() {
+        @Override
+        public void onBillingClientSetupFinished() {}
+
+        @Override
+        public void onConsumeFinished(String token, @BillingClient.BillingResponse int result) {
+          Log.e("token", token);
+        }
+
+        @Override
+        public void onPurchasesUpdated(List<Purchase> purchases) {
+          StringBuilder sb = new StringBuilder("[");
+
+          if (purchases != null) {
+            for (Purchase purchase : purchases) {
+              if (sb.length() > 1) {
+                sb.append(",");
+              }
+              sb.append("{");
+              sb.append("\"signature\":\"" + p.getSignature() + "\",");
+              sb.append("\"originalJson\":\"" + p.getOriginalJson() + "\",");
+              sb.append("\"sku\":\"" + p.getSku() + "\"");
+              sb.append("}");
+            }
+          }
+          sb.append("]");
+          result.success("{\"status\":\"loaded\",\"purchases\":" + sb.toString() + "}");
+        }
+      }, null);
+    } else if (call.method.equals("buy")) {
+      billingManager = new BillingManager(activity, new BillingManager.BillingUpdatesListener() {
+        @Override
+        public void onBillingClientSetupFinished() {
+          billingManager.initiatePurchaseFlow((String) call.arguments, BillingClient.SkuType.INAPP);
+        }
+
+        @Override
+        public void onConsumeFinished(String token, @BillingClient.BillingResponse int result) {
+          Log.e("token", token);
+        }
+
+        @Override
+        public void onPurchasesUpdated(List<Purchase> purchases) {
+          Log.e("purchases", purchases.toString());
+          if (purchases.size() > 0) {
+            Purchase p = purchases.get(0);
+            Log.e("Consuming", p.getSku());
+            billingManager.consumeAsync(p.getPurchaseToken());
+
+            StringBuilder sb = new StringBuilder("{");
+            sb.append("\"signature\":\"" + p.getSignature() + "\",");
+            sb.append("\"originalJson\":\"" + p.getOriginalJson() + "\",");
+            sb.append("\"productIdentifier\":\"" + p.getSku() + "\"");
+            sb.append("}");
+
+            result.success("{\"status\":\"loaded\",\"products\":" + sb.toString() + "}");
+          } else {
+            result.error("ERROR", "Failed to buy", null);
+          }
+        }
+      }, null);
+    } else if (call.method.equals("fetch")) { // gets products info
+      billingManager = new BillingManager(activity, new BillingManager.BillingUpdatesListener() {
+        @Override
+        public void onBillingClientSetupFinished() {
+          billingManager.querySKUProducts((List<String>) call.arguments);
+        }
+
+        @Override
+        public void onConsumeFinished(String token, int result) {}
+
+        @Override
+        public void onPurchasesUpdated(List<Purchase> purchases) {}
+      }, new SkuDetailsResponseListener() {
+        @Override
+        public void onSkuDetailsResponse(int responseCode, List<SkuDetails> skuDetailsList) {
+          StringBuilder sb = new StringBuilder("[");
+          if (skuDetailsList != null) {
+            for (SkuDetails details : skuDetailsList) {
+              if (sb.length() > 1) {
+                sb.append(",");
+              }
+              sb.append("{");
+              sb.append("\"localizedDescription\":\"" + details.getDescription() + "\",");
+              sb.append("\"localizedTitle\":\"" + details.getTitle() + "\",");
+              sb.append("\"price\":\"" + details.getPrice() + "\",");
+              sb.append("\"priceLocale\":\"" + details.getPriceCurrencyCode() + "\",");
+              sb.append("\"localizedPrice\":\"" + details.getPrice() + "\",");
+              sb.append("\"productIdentifier\":\"" + details.getSku() + "\"");
+              sb.append("}");
+            }
+          }
+          sb.append("]");
+          result.success("{\"status\":\"loaded\",\"products\":" + sb.toString() + "}");
+        }
+
+      });
+    } else {
+      result.notImplemented();
     }
+  }
+
+  public String jsonFromString(String status) {
+    return "{\"status\":\"" + status + "\"}";
+  }
 }
--- lib/flutter_iap.dart
+++ lib/flutter_iap.dart
@@ -6,42 +6,37 @@ import 'package:flutter/services.dart';
 class FlutterIap {
   static const MethodChannel _channel = const MethodChannel('flutter_iap');
 
-  /// Android only: Retrieve a list of SKUs which have been purchased previously.
-  static Future<List<String>> fetchInventory() async {
+  /// Android only: Retrieve a list of products which have been purchased previously.
+  static Future<IAPResponse> fetchInventory() async {
     String result = await _channel.invokeMethod("inventory");
-    Map<String, dynamic> map = json.decode(result);
-
-    List<dynamic> dynamicList = map["skus"];
-    return Future.value(dynamicList.map<String>((f) => f.toString()).toList());
+    return new IAPResponse(result);
   }
 
   /// Retrieve a list of products available for purchase.
   static Future<IAPResponse> fetchProducts(List<String> ids) async {
     String result = await _channel.invokeMethod("fetch", ids);
-    return stringToResponse(result);
+    return new IAPResponse(result);
   }
 
   /// Starts the purchase process
   static Future<IAPResponse> buy(String id) async {
     String result = await _channel.invokeMethod("buy", id);
-    return stringToResponse(result);
+    return new IAPResponse(result);
   }
 
   /// iOS only: Restore previous purchases
   static Future<IAPResponse> restorePurchases() async {
     String result = await _channel.invokeMethod("restore");
-    return stringToResponse(result);
-  }
-
-  static IAPResponse stringToResponse(String response) {
-    Map<String, dynamic> mapResponse = json.decode(response);
-    return new IAPResponse.fromJSON(mapResponse);
+    return new IAPResponse(result);
   }
 }
 
 class IAPResponse {
-  String status;
+  final String status;
   List<IAPProduct> products;
+  List<IAPPurchase> purchases;
+
+  factory IAPResponse(String source) => new IAPResponse.fromJSON(json.decode(source));
 
   IAPResponse.fromJSON(Map<String, dynamic> json) : status = json['status'] {
     if (json['products'] != null) {
@@ -49,7 +44,23 @@ class IAPResponse {
           .map<IAPProduct>((product) => new IAPProduct.fromJSON(product))
           .toList();
     }
+    if (json['purchases'] != null) {
+      purchases = json['purchases']
+          .map<IAPPurchase>((purchase) => new IAPPurchase.fromJSON(purchase))
+          .toList();
+    }
   }
+}
+
+class IAPPurchase {
+  final String productIdentifier;
+  final String signature;
+  final String originalJson;
+
+  IAPPurchase.fromJSON(Map<String, dynamic> json)
+      : productIdentifier = json['productIdentifier'],
+        signature = json['signature'],
+        originalJson = json['originalJson'];
 }
 
 class IAPProduct {
