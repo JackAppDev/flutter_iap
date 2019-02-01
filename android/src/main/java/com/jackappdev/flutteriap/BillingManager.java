85eda38e42fad2737970c4448a3429b6c7eaac73
Sebastian Roth <sebastian.roth@gmail.com>
2019-02-01 23:38:45 +0800

    AndroidX support

--- android/src/main/java/com/jackappdev/flutteriap/BillingManager.java
+++ android/src/main/java/com/jackappdev/flutteriap/BillingManager.java
@@ -1,8 +1,8 @@
 package com.jackappdev.flutteriap;
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.annotation.NonNull;
+
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.android.billingclient.api.BillingClient;
